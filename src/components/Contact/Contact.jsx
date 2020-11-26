16e645103d88fbe0a8e5f155bccc4cb78e6ede50
Nitish <41735985+nitishr7@users.noreply.github.com>
2020-11-27 01:33:50 +0530

    Enter

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,15 +15,15 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
+              {cta || ''}
             </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              href={email ? `mailto:${email}` : ''}
             >
-              {btn || "Let's Talk"}
+              {btn || ""}
             </a>
           </div>
         </Fade>
--- src/components/GithubButtons/GithubButtons.jsx
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -5,21 +5,21 @@ const GithubButton = () => (
   <>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio/fork"
+      href=""
       data-icon="octicon-repo-forked"
       data-size="large"
       data-show-count="true"
-      aria-label="Fork cobidev/gatsby-simplefolio on GitHub"
+      aria-label=""
     >
       Fork
     </GitHubButton>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio"
+      href=""
       data-icon="octicon-star"
       data-size="large"
       data-show-count="true"
-      aria-label="Star cobidev/gatsby-simplefolio on GitHub"
+      aria-label=""
     >
       Star
     </GitHubButton>
